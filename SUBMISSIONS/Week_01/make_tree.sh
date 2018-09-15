561b588dbba1a4728217cf356d4d28e2895240da
Youzhi Wu <yzwu1209@berkeley.edu>
2018-09-15 14:19:54 -0400

    remove cd commands

--- SUBMISSIONS/Week_01/make_tree.sh
+++ SUBMISSIONS/Week_01/make_tree.sh
@@ -1,23 +1,15 @@
 
 #!/bin/bash
 mkdir s1
-cd s1
-mkdir s2
-mkdir s3
-cd s3
-touch conf.txt
-cd ../s2
-touch text_chunk1.txt
-mkdir Advanced
-cd Advanced/
-touch text_chunk2.txt
-cd ..
-cd ../s3
-echo "I love bash scripting." >> conf.txt
-cd ../s2
-echo "A whole new world" >> text_chunk1.txt
-echo "A new fantastic point of view" >> text_chunk1.txt
-cd Advanced/
-cat ../text_chunk1.txt > text_chunk2.txt
-echo "Do you want to build a snowman?" >> text_chunk2.txt
+mkdir s1/s2
+mkdir s1/s3
+touch s1/s3/conf.txt
+touch s1/s2/text_chunk1.txt
+mkdir s1/s2/Advanced
+touch s1/s2/Advanced/text_chunk2.txt
+echo "I love bash scripting." >> s1/s3/conf.txt
+echo "A whole new world" >> s1/s2/text_chunk1.txt
+echo "A new fantastic point of view" >> s1/s2/text_chunk1.txt
+cat s1/s2/text_chunk1.txt > s1/s2/Advanced/text_chunk2.txt
+echo "Do you want to build a snowman?" >> s1/s2/Advanced/text_chunk2.txt
 
